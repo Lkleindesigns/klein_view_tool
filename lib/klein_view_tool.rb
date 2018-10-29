abbbcd412a2cf1ebbb85a40ba0a8bd751a5c5341
Logan Klein <Lkleindesigns@gmail.com>
2018-10-28 21:14:45 -0500

    View tool initial commit

---
+++ lib/klein_view_tool.rb
@@ -0,0 +1,6 @@
+require "klein_view_tool/version"
+require "klein_view_tool/renderer"
+
+module KleinViewTool
+  # Your code goes here...
+end
---
+++ lib/klein_view_tool/version.rb
@@ -0,0 +1,3 @@
+module KleinViewTool
+  VERSION = "0.1.0"
+end
